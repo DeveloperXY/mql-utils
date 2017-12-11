afcfa9c74f687d231eed0f573a328d3c1769d03a
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-12-11 11:37:07 +0000

    Create a convenience class to implement the BasicAction interface without having to implement any of its methods

--- src/main/java/org/mql/jee/web/action/AbstractAction.java
+++ src/main/java/org/mql/jee/web/action/AbstractAction.java
@@ -1,12 +1,28 @@
 package org.mql.jee.web.action;
 
 /**
- * @param <T> the type of the result returned by the action's methods.
+ * A convenience class to implement the BasicAction interface without having to implement any of its methods.
+ *
  * @author Mohammed Aouf ZOUAG, on 12/11/2017
  */
-public interface AbstractAction<T> {
-    T add();
-    T update();
-    T delete();
-    T list();
+public abstract class AbstractAction<T> implements BasicAction<T> {
+    @Override
+    public T add() {
+        throw new UnsupportedOperationException("AbstractAction.add() isn't supported.");
+    }
+
+    @Override
+    public T update() {
+        throw new UnsupportedOperationException("AbstractAction.update() isn't supported.");
+    }
+
+    @Override
+    public T delete() {
+        throw new UnsupportedOperationException("AbstractAction.delete() isn't supported.");
+    }
+
+    @Override
+    public T list() {
+        throw new UnsupportedOperationException("AbstractAction.list() isn't supported.");
+    }
 }
