679b8e25158d9e984888fa0d9ad5c06a093d3e3d
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-12-11 10:51:12 +0000

    Create the first version of the ActionProcecssor.

---
+++ src/main/java/org/mql/processors/ActionProcessor.java
@@ -0,0 +1,29 @@
+package org.mql.processors;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes("org.mql.jee.annotations.Action")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class ActionProcessor extends AbstractProcessor {
+
+    private TypeElement actionElement;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        actionElement = processingEnv.getElementUtils().getTypeElement("org.mql.jee.annotations.Action");
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        roundEnv.getElementsAnnotatedWith(actionElement)
+                .stream()
+                .map(element -> ((TypeElement) element))
+                .forEach(clazz -> System.out.println(clazz.getSimpleName().toString() + " is annotated with @Action"));
+
+        return false;
+    }
+}
