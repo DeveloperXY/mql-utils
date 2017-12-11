700b3d0fa893ff1cddd23a8b844f811e700d70e3
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2017-12-11 10:44:30 +0000

    Create the @Action annotation.

---
+++ src/main/java/org/mql/jee/annotations/Action.java
@@ -0,0 +1,10 @@
+package org.mql.jee.annotations;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Action {
+    String value();
+}
